b6a406974045a4634e533de3d332f797495fdefe
SMarander <137708107+SMarander@users.noreply.github.com>
2024-03-09 19:17:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// script.js
+
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+
+// Function to toggle the navigation menu on mobile devices
+function toggleNav() {
+    const nav = document.querySelector('nav ul');
+    nav.classList.toggle('active');
+}
+
+// Add an event listener to the menu icon for mobile devices
+const menuIcon = document.querySelector('#menu-icon');
+if (menuIcon) {
+    menuIcon.addEventListener('click', toggleNav);
+}
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 10) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
